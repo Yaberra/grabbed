03e400819f0f2e7193b6f4bce32097a9da1ba86d
unknown <yetia@yaberra.com>
2017-12-06 18:48:37 -0500

    added api code for google maps

--- assets/javascript/grabbed.js
+++ assets/javascript/grabbed.js
@@ -1,4 +1,99 @@
 $( document ).ready(function() {
     console.log( "test!" );
 
-});
+// This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+
+	var map, infoWindow;
+
+		function initMap() {
+  		map = new google.maps.Map(document.getElementById('map'), {
+    	center: {lat: -34.397, lng: 150.644},
+    	zoom: 6
+  		});
+  		infoWindow = new google.maps.InfoWindow;
+
+ // Try HTML5 geolocation.
+
+  	if (navigator.geolocation) {
+    	navigator.geolocation.getCurrentPosition(function(position) {
+      	var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      	};
+
+      	infoWindow.setPosition(pos);
+      	infoWindow.setContent('Location found.');
+      	infoWindow.open(map);
+      	map.setCenter(pos);
+    	}, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+    	});
+  		} else {
+    // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+        }
+       }
+
+       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+
+  });
+
+function getYelpRestaurantLocation(){
+
+}
+
+
+
+
+
+
+
+
+
+//   	function getRestaurants(){
+
+// 		var searchResults = $(this).data("search");
+
+// 		var queryURL ="https://api.yelp.com/v3/businesses/restaurants      "
+// 		}
+		
+// 		$.ajax({
+// 		url:queryURL
+// 		method;"GET"
+// 		}). done(function)(response){
+
+// 		var search = response.data;
+// 		for(i=0;i<results.length; i++);
+
+// 		var name = // response.restaurant.name	
+// 		var address = // response.restaurant.location.display_address 
+// 		var phone =	// response.restaurant.phone 
+// 		var rating =// response.restaurant.rating 
+// 		var review = // response.restaurant.review_count	
+// 		var price = // response.restaurant.price 
+// 		var deals= // response.restaurant.deals
+// 		var open =	// response.restaurant.open_now 
+// 		var website = // response.restaurant.url 
+
+
+
+	
+	
+		
+	
+	
+	
+	
+	
+
+
+// });

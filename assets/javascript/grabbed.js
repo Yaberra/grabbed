c5d15102f15a934fb63eca66f84e202cc8406430
unknown <yetia@yaberra.com>
2017-12-07 22:45:40 -0500

    ajax call to yelp and variables for pulling specific data from the call

--- assets/javascript/grabbed.js
+++ assets/javascript/grabbed.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-    console.log("test!");
-
     // This example requires that you consent to location sharing when
     // prompted by your browser. If you see the error "The Geolocation service
     // failed.", it means you probably did not give permission for the browser to
@@ -50,72 +48,48 @@ $(document).ready(function() {
 
     $(document).on('click', function() {
         console.log(pos)
-        
+
     })
 });
 
 // This is where we make the AJAX call to Yelp. 
 // We need to pass the google api location data to get restaurant location
 
-function getRestaurants(pos) {
-    var settings ={ "async": true,
-  "crossDomain": true,
+    function getRestaurants(pos) {
+        var settings = {
+        "async": true,
+        "crossDomain": true,
 
-  "url": `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?latitude=${pos.lat}&longitude=${pos.lng}`,
+        "url": `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?latitude=${pos.lat}&longitude=${pos.lng}`,
 
-  "url": 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=Atlanta',
+        "url": 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=Atlanta',
 
-  "method": "GET",
-  "headers": {
-    "authorization": "Bearer uM5_8KBZq5cZd5JzuR9SwUTy4KsRUE_qcnwuSjmBjloyvJ1ZU95AkFLcsccNSLQ7EX1WvndHjcfFXAzd40aevtIW0bfJvJQcqr1faSjidDUijEIb7d9Fycri1yAoWnYx",
-    "cache-control": "no-cache", 
-  }
-  }
-      $.ajax(settings).done(res => {
+        "method": "GET",
+        "headers": {
+            "authorization": "Bearer uM5_8KBZq5cZd5JzuR9SwUTy4KsRUE_qcnwuSjmBjloyvJ1ZU95AkFLcsccNSLQ7EX1WvndHjcfFXAzd40aevtIW0bfJvJQcqr1faSjidDUijEIb7d9Fycri1yAoWnYx",
+            "cache-control": "no-cache",
+        }
+    }
+    $.ajax(settings).done(res => {
         console.log('working in cb')
         console.log(res)
-      })
+    })
 }
 
-//var settings = {
-//   "async": true,
-//   "crossDomain": true,
-//   "url": "https://api.yelp.com/v3/businesses/search?term=restaurant&location=seattle",
-//   "method": "GET",
-//   "headers": {
-//     "Authorization": "Bearer uM5_8KBZq5cZd5JzuR9SwUTy4KsRUE_qcnwuSjmBjloyvJ1ZU95AkFLcsccNSLQ7EX1WvndHjcfFXAzd40aevtIW0bfJvJQcqr1faSjidDUijEIb7d9Fycri1yAoWnYx",
-//     "Cache-Control": "no-cache",
-//     "Postman-Token": "00a28000-9864-3346-e41b-49360c9c6c25"
-//   }
-// }
+    var results = res.data;
+    for (var i = 0; i < results.length; i++) {
 
-// $.ajax(settings).done(function (response) {
-//   console.log(response);
-// });
-    
-    //    var search = response.data;
-    //    for(i=0;i<results.length; i++);
+    var imageThumbnail = $("<div class='col-md-4'>");
+    var restaurantImage = $("<img>");
+    var imageUrl = results[i].image_url;
+    restaurantImage.attr("src", imageUrl)
 
-    //    var name = // response.restaurant.name  
-    //    var address = // response.restaurant.location.display_address 
-    //    var phone = // response.restaurant.phone 
-    //    var rating =// response.restaurant.rating 
-    //    var review = // response.restaurant.review_count  
-    //    var price = // response.restaurant.price 
-    //    var deals= // response.restaurant.deals
-    //    var open =  // response.restaurant.open_now 
-    //    var website = // response.restaurant.url 
+    var businessesSearchResults = $("<div class='col-md-8'>");
+    var name = results[i].name;
+    console.log(name);
+    var rating = $("<p>").text("Rating: " + rating);
+    var phone = results[i].display_phone;
+    var address = results[i].display_address;
+    var website = results[i].url;
 
-
-
-
-
-
-
-
-
-
-
-
-
-    // });
+}
